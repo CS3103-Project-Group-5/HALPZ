afcb85e136a7b8129c10bbdaa3589c03965323e5
Qin_Shihuang <cafeboss@live.cn>
2017-11-02 15:49:44 +0800

    static peer class, start function

--- Client.java
+++ Client.java
@@ -6,98 +6,174 @@ import java.net.*;
 
 public class Client {
 
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-        Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 
-        // Wait for a request of user
-        int option = -1, chunkSize = 256 * 1024;
-        String input;
-        long fileSize;
-        ArrayList<PeerInfo> peerList;
-        BitSet chunkList;
-        TrackerMessage.MODE mode;
+		// Wait for a request of user
+		int option = -1, chunkSize = 256 * 1024;
+		String input;
+		long fileSize;
+		ArrayList<PeerInfo> peerList;
+		BitSet chunkList;
+		TrackerMessage.MODE mode;
 
-        while (option != 5) {
-            System.out.println("\nSelect an option:");
-            System.out.println("1. Query the centralised server for list of files available.");
-            System.out.println("2. Query the centralised server for a specific file.");
-            System.out.println("3. Download a file by specifying the filename.");
-            System.out.println("4. Inform availability of a new file.");
-            System.out.println("5. Exit.\n");
+		while (option != 5) {
+			System.out.println("\nSelect an option:");
+			System.out.println("1. Query the centralised server for list of files available.");
+			System.out.println("2. Query the centralised server for a specific file.");
+			System.out.println("3. Download a file by specifying the filename.");
+			System.out.println("4. Inform availability of a new file.");
+			System.out.println("5. Exit.\n");
 
-            try {
-                option = scanner.nextInt();
-                scanner.nextLine();
-            } catch (Exception e) {
-                System.out.println("Please enter a valid command.");
-                scanner.nextLine();
-                continue;
-            }
-            switch (option) {
-                case 1:
-                    try {
-                        printFileList(TrackerManager.getFileList());
-                        break;
-                    } catch (Exception e) {
-                        System.out.println("Tracker error");
-                        e.printStackTrace();
-                        break;
-                    }
-                case 2:
-                    System.out.println("Please input file name.");
-                    input = scanner.nextLine();
-                    fileSize = TrackerManager.getFileSize(input);
+			try {
+				option = scanner.nextInt();
+				scanner.nextLine();
+			} catch (Exception e) {
+				System.out.println("Please enter a valid command.");
+				scanner.nextLine();
+				continue;
+			}
+			switch (option) {
+				case 1:
+					try {
+						printFileList(TrackerManager.getFileList());
+						break;
+					} catch (Exception e) {
+						System.out.println("Tracker error");
+						e.printStackTrace();
+						break;
+					}
+				case 2:
+					System.out.println("Please input file name.");
+					input = scanner.nextLine();
+					fileSize = TrackerManager.getFileSize(input);
 					if (fileSize == -1) {
-                    	System.out.println("File does not exist.");
+						System.out.println("File does not exist.");
 					} else {
-                    	System.out.println("Filename: " + input + ", Filesize: " + fileSize);
-                    }
+						System.out.println("Filename: " + input + ", Filesize: " + fileSize);
+					}
 					break;
 
-                case 3:
-                    System.out.println("Please input file name.");
-                    input = scanner.nextLine();
-                    TrackerMessage msg = TrackerManager.getDownloadInfo(input);
-                    fileSize = msg.getFileSize();
-                    peerList = msg.getPeerList();
-                    chunkList = new BitSet((int) Math.ceil(fileSize / (double) chunkSize)); // <-- need to load file
-                    /* call peer class to do the p2p */
-                    break;
+				case 3:
+					System.out.println("Please input file name.");
+					input = scanner.nextLine();
+					TrackerMessage msg = TrackerManager.getDownloadInfo(input);
+					fileSize = msg.getFileSize();
+					peerList = msg.getPeerList();
+					chunkList = new BitSet((int) Math.ceil(fileSize / (double) chunkSize)); // <-- need to load file
+					/* call peer class to do the p2p */
+					break;
 
-                case 4:
-                    System.out.println("Please input location of file to upload.");
-                    input = scanner.nextLine();
-                    File file = new File(input);
-                    if (!file.exists()) {
-                        System.out.println("File not found, exiting...");
-                        break;
-                    }
-                    fileSize = file.length();
-                    chunkList = new BitSet((int) Math.ceil(fileSize / (double) chunkSize));
-                    chunkList.flip(0, chunkList.length());
-                    TrackerManager.initializeUpload(input, fileSize);
-                    break;
+				case 4:
+					System.out.println("Please input location of file to upload.");
+					input = scanner.nextLine();
+					File file = new File(input);
+					if (!file.exists()) {
+						System.out.println("File not found, exiting...");
+						break;
+					}
+					fileSize = file.length();
+					chunkList = new BitSet((int) Math.ceil(fileSize / (double) chunkSize));
+					chunkList.flip(0, chunkList.length());
+					TrackerManager.initializeUpload(input, fileSize);
+					break;
 
-            }
-        }
+			}
+		}
 
-        scanner.close();
-    }
+		scanner.close();
+	}
 
-    /* Helper Methods */
-    private static long generateID() {
-        Random rnd = new Random(506);
-        return rnd.nextLong();
-    }
+	private static long generateID() {
+		Random rnd = new Random(506);
+		return rnd.nextLong();
+	}
 
-    private static void printFileList(Set<String> list) {
-        System.out.println("File list: ");
-        for (String s : list) {
-            System.out.println(s);
-        }
-    }
+	private static void printFileList(Set<String> list) {
+		System.out.println("File list: ");
+		for (String s : list) {
+			System.out.println(s);
+		}
+	}
+
+	private static void start(ArrayList<PeerInfo> list) {
+		for (PeerInfo info : list) {
+			try {
+				new Thread(new Peer(new Socket(info.getPeerIP(), info.getPeerPort()))).start();
+			} catch (Exception e) {
+				e.printStackTrace();
+				continue;
+			}
+		}
+
+		ServerSocket welcomeSocket;
+		try {
+			welcomeSocket = new ServerSocket(welcomePort);
+		} catch (Exception e) {
+			System.out.println("Welcome port in use.");
+			e.printStackTrace();
+			return;
+		} 
+
+		while (true) {
+			try {
+				Socket connectionSocket = welcomeSocket.accept();
+				System.out.println("New peer connected " + connectionSocket.getInetAddress().getHostAddress());
+				new Thread(new Peer(connectionSocket)).start();
+			} catch (Exception e) {
+				e.printStackTrace();
+				continue;
+			}
+		}
+	}
+
+	static class Peer implements Runnable {
+		private Socket socket;
+		private ObjectOutputStream out;
+		private ObjectInputStream in;
+
+		public Peer(Socket socket) {
+			socket = socket;
+		}
+
+		public void run() {
+		}
+
+		private void sendChunkList() {
+		}
+
+		private void receiveChunkList() {
+		}
+
+		private void sendChunkRequest() {
+		}
+
+		private void sendChunks() {
+		}
+
+		private void receiveChunks() {
+		}
+
+		private ArrayList<Integer> getDesiredChunkList() {
+			return null;
+		}
+
+		private void writeToFile() {
+		}
+
+		private void readFromFile() {
+		}
+
+		private int getChunkOffset(int number) {
+			return 0;
+		}
+
+		private void removeFileStatus() {
+		}
+	}
 }
+
 class TrackerManager {
 
 	private static final String TRACKER_ADDRESS = "128.199.108.79";
@@ -115,7 +191,7 @@ class TrackerManager {
 	private void close() throws Exception {
 		socket.close();
 	}
-	
+
 	private void send(TrackerMessage msg) throws Exception {
 		out.writeObject(msg);
 	}
